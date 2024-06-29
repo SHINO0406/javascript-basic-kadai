ea3fd7d30afb51a657767da5f42e7753d1ab9a13
SHINO0406 <es.h146.j20@gmail.com>
2024-06-29 11:24:58 +0900

    Update bmi.js

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,8 @@
 // 変数の宣言・値の代入
 let userWeight = 68;
 let userHeight = 1.7;
+let userBmi = userWeight/(userHeight*userHeight);
 
 // コンソールへの出力
-console.log(userWeight/(userHeight*userHeight));
+console.log(userBmi);
+
